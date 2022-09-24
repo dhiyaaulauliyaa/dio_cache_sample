3acb8e4618be2cb235d123378b034e72dd27154c
Dhiyaaul Auliyaa <ahmad.auliyaa@stockbit.com>
2022-09-24 13:14:26 +0700

    feat(core-constants): :sparkles: Init constants files

---
+++ lib/core/constants/sizing_const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SizingConst {
+  static double get defaultPadding => 18.sp;
+  static double get childPadding => 16.sp;
+  static double get innerPadding => 8.sp;
+  static double get defaultIconSize => 24.sp;
+}
